1ea32b11f2c601eb61367d7bf4530bdc1cf8a915
PeeHaa <info@pieterhordijk.com>
2013-04-27 12:41:17 +0200

    FIX ALL THE NAMESPACES

--- src/PitchBlade/Core/Autoloader.php
+++ src/PitchBlade/Core/Autoloader.php
@@ -11,7 +11,7 @@
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    1.0.0
  */
-namespace EZToolsLib\Core;
+namespace PitchBlade\Core;
 
 /**
  * PSR-0 Autoloader
