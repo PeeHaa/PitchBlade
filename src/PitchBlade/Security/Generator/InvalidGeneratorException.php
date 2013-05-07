ae1c3db702da41d8b8f3d59e6022acd7a6d740dc
PeeHaa <info@pieterhordijk.com>
2013-05-07 23:02:00 +0200

    Added invalid generator exception

---
+++ src/PitchBlade/Security/Generator/InvalidGeneratorException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when the factory tries to build an invalid generator
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Security\Generator;
+
+/**
+ * Exception which gets thrown when the factory tries to build an invalid generator
+ *
+ * @category   PitchBlade
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidGeneratorException extends \Exception
+{
+}
