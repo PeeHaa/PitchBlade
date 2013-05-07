3f49d748bdf8498efc3c9434a3e0d698bf493a36
PeeHaa <info@pieterhordijk.com>
2013-05-07 20:49:42 +0200

    Added exception for unsupported algos

---
+++ src/PitchBlade/Security/Generator/UnsupportedAlgorithmException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when an unsupported algorithm is used
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Security\Generator;
+
+/**
+ * Exception which gets thrown when an unsupported algorithm is used
+ *
+ * @category   PitchBlade
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class UnsupportedAlgorithmException extends \Exception
+{
+}
