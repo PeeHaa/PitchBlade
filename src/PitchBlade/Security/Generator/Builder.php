d9b11fa4bb401e75f90f9ef8fbd05f62080bb760
PeeHaa <info@pieterhordijk.com>
2013-05-07 20:48:12 +0200

    Added random string generator interface

---
+++ src/PitchBlade/Security/Generator/Builder.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Interface for generator factories
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Security\Generator;
+
+/**
+ * Interface for generator factories
+ *
+ * @category   PitchBlade
+ * @package    Security
+ * @subpackage Generator
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+interface Builder
+{
+    /**
+     * Builds a random string generator
+     *
+     * @param string $class The fully qualified class name
+     *
+     * @return \PitchBlade\Security\Generator The random string generator requested
+     * @throws \PitchBlade\Security\Generator\InvalidGeneratorException If the generator can not be loaded
+     */
+    public function build($class);
+}
