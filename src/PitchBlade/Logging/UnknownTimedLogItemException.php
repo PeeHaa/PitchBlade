6e25b8ed56a0ba8458913c8b85300c54678a454b
PeeHaa <info@pieterhordijk.com>
2013-05-09 17:04:40 +0200

    Added invalid id for timed logger exception

---
+++ src/PitchBlade/Logging/UnknownTimedLogItemException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when the timed logger receive an invalid id
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Logging
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Logging;
+
+/**
+ * Exception which gets thrown when the timed logger receive an invalid id
+ *
+ * @category   PitchBlade
+ * @package    Logging
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class UnknownTimedLogItemException extends \Exception
+{
+}
