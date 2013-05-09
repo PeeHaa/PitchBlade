932cc94b5b8d6ebe2d4cd90fbc02112b693989a0
PeeHaa <info@pieterhordijk.com>
2013-05-09 17:03:15 +0200

    Added timed logs

---
+++ src/PitchBlade/Logging/TimedLogger.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Provides a logger which has a start time and a end time
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Logging
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Logging;
+
+use PitchBlade\Logging\Logger;
+
+/**
+ * This interface represents a logger
+ *
+ * @category   PitchBlade
+ * @package    Logging
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class TimedLogger
+{
+    /**
+     * @var array List of the timed logs based on the unique identifiers
+     */
+    private $timedLogs = [];
+
+    /**
+     * @var \PitchBlade\Logging\Logger The logger
+     */
+    private $logger;
+
+    /**
+     * Creates instance
+     *
+     * @param \PitchBlade\Logging\Logger The logger
+     */
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Starts the timer to log the current action
+     *
+     * @return int The unique identifier of the current log item
+     */
+    public function start()
+    {
+        $uniqueIdentifier = uniqid('', true);
+
+        $this->timedLogs[$uniqueIdentifier] = microtime(true);
+
+        return $uniqueIdentifier;
+    }
+
+    /**
+     * Ends the timer and logs the action
+     *
+     * @param int    $int    The unique identifier of the action to log
+     * @param string $type   The type of action which is being logged (e.g. PDO::query)
+     * @param string $action The specific action (e.g. 'SELECT * FROM table')
+     *
+     * @throws \PitchBlade\Logging\UnknownTimedLogItemException When an unknown id is supplied
+     */
+    public function end($id, $type, $action)
+    {
+        if (!array_key_exists($id, $this->timedLogs)) {
+            throw new UnknownTimedLogItemException(
+                'The supplied id (`' . $id . '`) doesn\'t match with a running timed log'
+            );
+        }
+
+        $this->logger->log($type, $action, (microtime(true) - $this->timedLogs[$id]));
+
+        unset($this->timedLogs[$id]);
+    }
+}
