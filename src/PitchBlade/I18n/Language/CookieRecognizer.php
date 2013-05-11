1c6255d402f94913a9f9917c76925f8d02da4c75
PeeHaa <info@pieterhordijk.com>
2013-05-11 16:18:13 +0200

    Added cookie recognizer

---
+++ src/PitchBlade/I18n/Language/CookieRecognizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Language recognizer based on the URL of the current request
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    I18n
+ * @subpackage Language
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\I18n\Language;
+
+use PitchBlade\I18n\Language\Recognizer,
+    PitchBlade\Http\RequestData;
+
+/**
+ * Language recognizer based on the URL of the current request
+ *
+ * @category   PitchBlade
+ * @package    I18n
+ * @subpackage Language
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class CookieRecognizer implements Recognizer
+{
+    /**
+     * @var array List of supported languages in the system
+     */
+    private $supportedLanguages;
+
+    /**
+     * @var \PitchBlade\Http\RequestData The request data
+     */
+    private $request;
+
+    /**
+     * Creates instance
+     *
+     * @param array                        $supportedLanguages The list of supported languages
+     * @param \PitchBlade\Http\RequestData $request            The request data
+     */
+    public function __construct(array $supportedLanguages, RequestData $request)
+    {
+        $this->supportedLanguages = $supportedLanguages;
+        $this->request            = $request;
+    }
+
+    /**
+     * Tries to retrieve the language
+     *
+     * @return null|string The language
+     */
+    public function getLanguage()
+    {
+        return $this->request->getCookieVariable('language');
+    }
+}
