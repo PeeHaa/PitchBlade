ce4bef78319f03e70b91af710611b8bb9c37afa8
PeeHaa <info@pieterhordijk.com>
2013-05-11 16:12:31 +0200

    Added invalid recognizer exception

---
+++ src/PitchBlade/I18n/Language/InvalidRecognizerException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when trying to build an invalid recognizer
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    I18n
+ * @subpackage Language
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\I18n\Language;
+
+/**
+ * Exception which gets thrown when trying to build an invalid recognizer
+ *
+ * @category   PitchBlade
+ * @package    I18n
+ * @subpackage Language
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidRecognizerException extends \Exception
+{
+}
