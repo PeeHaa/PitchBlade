fa7adf616e8ddde74905feef447290057a461173
PeeHaa <info@pieterhordijk.com>
2013-05-12 17:50:13 +0200

    Added autoloader test

---
+++ test/Unit/Core/AutoloaderTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace PitchBladeTest\Core;
+
+use PitchBlade\Core\Autoloader;
+
+class AutoloaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     */
+    public function testConstructCorrectInstance()
+    {
+        $autoloader = new Autoloader('Test', '/');
+
+        $this->assertInstanceOf('\\PitchBlade\\Core\\Autoloader', $autoloader);
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     */
+    public function testRegister()
+    {
+        $autoloader = new Autoloader('Test', '/');
+
+        $this->assertTrue($autoloader->register());
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     * @covers PitchBlade\Core\Autoloader::unregister
+     */
+    public function testUnregister()
+    {
+        $autoloader = new Autoloader('Test', '/');
+
+        $this->assertTrue($autoloader->register());
+        $this->assertTrue($autoloader->unregister());
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     * @covers PitchBlade\Core\Autoloader::load
+     */
+    public function testLoadSuccess()
+    {
+        $autoloader = new Autoloader('FakeProject', dirname(__DIR__) . '/../Mocks/Core');
+
+        $this->assertTrue($autoloader->register());
+
+        $someClass = new \FakeProject\NS\SomeClass();
+
+        $this->assertTrue($someClass->isLoaded());
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     * @covers PitchBlade\Core\Autoloader::load
+     */
+    public function testLoadSuccessExtraSlashedNamespace()
+    {
+        $autoloader = new Autoloader('\\\\FakeProject', dirname(__DIR__) . '/../Mocks/Core');
+
+        $this->assertTrue($autoloader->register());
+
+        $someClass = new \FakeProject\NS\SomeClass();
+
+        $this->assertTrue($someClass->isLoaded());
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     * @covers PitchBlade\Core\Autoloader::load
+     */
+    public function testLoadSuccessExtraForwardSlashedPath()
+    {
+        $autoloader = new Autoloader('FakeProject', dirname(__DIR__) . '/../Mocks/Core//');
+
+        $this->assertTrue($autoloader->register());
+
+        $someClass = new \FakeProject\NS\SomeClass();
+
+        $this->assertTrue($someClass->isLoaded());
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     * @covers PitchBlade\Core\Autoloader::load
+     */
+    public function testLoadSuccessExtraBackwardSlashedPath()
+    {
+        $autoloader = new Autoloader('FakeProject', dirname(__DIR__) . '/../Mocks/Core\\');
+
+        $this->assertTrue($autoloader->register());
+
+        $someClass = new \FakeProject\NS\SomeClass();
+
+        $this->assertTrue($someClass->isLoaded());
+    }
+
+    /**
+     * @covers PitchBlade\Core\Autoloader::__construct
+     * @covers PitchBlade\Core\Autoloader::register
+     * @covers PitchBlade\Core\Autoloader::load
+     */
+    public function testLoadSuccessExtraMixedSlashedPath()
+    {
+        $autoloader = new Autoloader('FakeProject', dirname(__DIR__) . '/../Mocks/Core\\\\/\\//');
+
+        $this->assertTrue($autoloader->register());
+
+        $someClass = new \FakeProject\NS\SomeClass();
+
+        $this->assertTrue($someClass->isLoaded());
+    }
+}
