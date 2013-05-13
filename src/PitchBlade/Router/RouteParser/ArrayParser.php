76523da99a4fdd5bbe375247bb6f59749d9f9d9f
PeeHaa <info@pieterhordijk.com>
2013-05-13 20:27:38 +0200

    Added array routes parser

---
+++ src/PitchBlade/Router/RouteParser/ArrayParser.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Routes parser based on array input. This class adds routes to a routes collection
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @subpackage RouteParser
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Router\RouteParser;
+
+use PitchBlade\Router\RouteParser\Parser,
+    PitchBlade\Router\Routable;
+
+/**
+ * Routes parser based on array input. This class adds routes to a routes collection
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @subpackage RouteParser
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class ArrayParser implements Parser
+{
+    /**
+     * @var \PitchBlade\Router\Routable The routes collection
+     */
+    private $routesCollection;
+
+    /**
+     * Creates instance
+     *
+     * @param \PitchBlade\Router\Routable $routesCollection The routes collection
+     */
+    public function __construct(Routable $routesCollection)
+    {
+        $this->routesCollection = $routesCollection;
+    }
+
+    /**
+     * Parses the routes formatted as an array
+     *
+     * @param array $routes The routes
+     */
+    public function parse(array $routes)
+    {
+        foreach ($routes as $name => $route) {
+            if (array_key_exists('mapping', $route)) {
+                $this->routesCollection->add(
+                    $name,
+                    $route['requirements'],
+                    $route['view'],
+                    $route['controller'],
+                    $route['mapping']
+                );
+            } else {
+                $this->routesCollection->add(
+                    $name,
+                    $route['requirements'],
+                    $route['view'],
+                    $route['controller']
+                );
+            }
+        }
+    }
+}
