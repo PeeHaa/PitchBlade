76821b24b6e7a747677e1751f6a19f54b1f266a9
PeeHaa <info@pieterhordijk.com>
2013-05-13 23:49:42 +0200

    When I die I want this code to go to @DaveRandom

--- test/Unit/Logging/ArrayLoggerTest.php
+++ test/Unit/Logging/ArrayLoggerTest.php
@@ -77,7 +77,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertNull($logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/', $logItems[0]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y H:i:s', $logItems[0]['time'])->format('d-m-Y H:i:s'), $logItems[0]['time']);
     }
 
     /**
@@ -97,7 +97,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertNull($logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4}$/', $logItems[0]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y', $logItems[0]['time'])->format('d-m-Y'), $logItems[0]['time']);
     }
 
     /**
@@ -117,7 +117,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertSame(10, $logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/', $logItems[0]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y H:i:s', $logItems[0]['time'])->format('d-m-Y H:i:s'), $logItems[0]['time']);
     }
 
     /**
@@ -125,7 +125,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
      * @covers PitchBlade\Logging\ArrayLogger::log
      * @covers PitchBlade\Logging\ArrayLogger::report
      */
-    public function testReportCustomDefaultFormatWithExecutionTime()
+    public function testReportCustomFormatWithExecutionTime()
     {
         $logger = new ArrayLogger('d-m-Y');
 
@@ -137,7 +137,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertSame(10, $logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4}$/', $logItems[0]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y', $logItems[0]['time'])->format('d-m-Y'), $logItems[0]['time']);
     }
 
     /**
@@ -159,16 +159,16 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('firstType', $logItems[0]['type']);
         $this->assertSame('firstAction', $logItems[0]['action']);
         $this->assertNull($logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/', $logItems[0]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y H:i:s', $logItems[0]['time'])->format('d-m-Y H:i:s'), $logItems[0]['time']);
 
         $this->assertSame('secondType', $logItems[1]['type']);
         $this->assertSame('secondAction', $logItems[1]['action']);
         $this->assertSame(10, $logItems[1]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/', $logItems[1]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y H:i:s', $logItems[0]['time'])->format('d-m-Y H:i:s'), $logItems[1]['time']);
 
         $this->assertSame('thirdType', $logItems[2]['type']);
         $this->assertSame('thirdAction', $logItems[2]['action']);
         $this->assertSame(99, $logItems[2]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4} ([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/', $logItems[2]['time']));
+        $this->assertSame(\DateTime::createFromFormat('d-m-Y H:i:s', $logItems[0]['time'])->format('d-m-Y H:i:s'), $logItems[2]['time']);
     }
 }
