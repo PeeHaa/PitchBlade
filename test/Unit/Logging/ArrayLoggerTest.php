4470c31edd1ae97eee0f6aed737ca437c3548113
PeeHaa <info@pieterhordijk.com>
2013-05-13 23:37:50 +0200

    Ain't nobody got time for months with 32 days in it

--- test/Unit/Logging/ArrayLoggerTest.php
+++ test/Unit/Logging/ArrayLoggerTest.php
@@ -77,8 +77,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertNull($logItems[0]['executionTime']);
-        //var_dump($logItems[0]['time']);die;
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[0]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[0]['time']));
     }
 
     /**
@@ -98,7 +97,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertNull($logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4}$/', $logItems[0]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4}$/', $logItems[0]['time']));
     }
 
     /**
@@ -118,7 +117,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertSame(10, $logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[0]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[0]['time']));
     }
 
     /**
@@ -138,7 +137,7 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('someType', $logItems[0]['type']);
         $this->assertSame('someAction', $logItems[0]['action']);
         $this->assertSame(10, $logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4}$/', $logItems[0]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4}$/', $logItems[0]['time']));
     }
 
     /**
@@ -160,16 +159,16 @@ class ArrayLoggerTest extends \PHPUnit_Framework_TestCase
         $this->assertSame('firstType', $logItems[0]['type']);
         $this->assertSame('firstAction', $logItems[0]['action']);
         $this->assertNull($logItems[0]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[0]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[0]['time']));
 
         $this->assertSame('secondType', $logItems[1]['type']);
         $this->assertSame('secondAction', $logItems[1]['action']);
         $this->assertSame(10, $logItems[1]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[1]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[1]['time']));
 
         $this->assertSame('thirdType', $logItems[2]['type']);
         $this->assertSame('thirdAction', $logItems[2]['action']);
         $this->assertSame(99, $logItems[2]['executionTime']);
-        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[12])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[2]['time']));
+        $this->assertSame(1, preg_match('/^(0[1-9]|[12][\d]|3[01])-(0[1-9]|1[0-2])-[\d]{4} (0[1-9]|1[\d]|2[0-3]):(0[1-9]|[1-5][\d]):(0[1-9]|[1-5][\d])$/', $logItems[2]['time']));
     }
 }
