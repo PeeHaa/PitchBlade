95d70b95d07c35f04bdc4462e9fe242dd4b79ae4
PeeHaa <info@pieterhordijk.com>
2013-05-16 23:01:17 +0200

    Implemented dummy view mock

--- test/Mocks/Mvc/View/DummyView.php
+++ test/Mocks/Mvc/View/DummyView.php
@@ -2,8 +2,19 @@
 
 namespace PitchBladeTest\Mocks\Mvc\View;
 
-use PitchBlade\Mvc\View\Viewable;
+use PitchBlade\Mvc\View\Viewable,
+    PitchBlade\Mvc\View\Builder,
+    PitchBlade\Mvc\Model\ServiceBuilder,
+    PitchBlade\I18n\Translator;
 
 class DummyView implements Viewable
 {
+    public function __construct(
+        Builder $viewFactory,
+        ServiceBuilder $serviceFactory,
+        Translator $translator,
+        $baseTemplate
+    )
+    {
+    }
 }
