399e6c325ee1c54c04756cf4fb9f96fddef63bb5
PeeHaa <info@pieterhordijk.com>
2013-05-18 17:31:59 +0200

    Added invalid template exception

---
+++ src/PitchBlade/Mvc/View/InvalidTemplateException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when trying to render an invalid template
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Mvc
+ * @subpackage Model
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Mvc\View;
+
+/**
+ * Exception which gets thrown when trying to render an invalid template
+ *
+ * @category   PitchBlade
+ * @package    Mvc
+ * @subpackage Model
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidTemplateException extends \Exception
+{
+}
