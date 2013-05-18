c0fa0dd89e066ab4393795e819763724965f0b51
PeeHaa <info@pieterhordijk.com>
2013-05-18 23:30:43 +0200

    Added mock methods to view

--- test/Mocks/Mvc/View/DummyView.php
+++ test/Mocks/Mvc/View/DummyView.php
@@ -2,19 +2,47 @@
 
 namespace PitchBladeTest\Mocks\Mvc\View;
 
-use PitchBlade\Mvc\View\Viewable,
+use PitchBlade\Mvc\View\View,
+    PitchBlade\Mvc\View\Viewable,
     PitchBlade\Mvc\View\Builder,
     PitchBlade\Mvc\Model\ServiceBuilder,
     PitchBlade\I18n\Translator;
 
-class DummyView implements Viewable
+class DummyView extends View implements Viewable
 {
     public function __construct(
         Builder $viewFactory,
         ServiceBuilder $serviceFactory,
         Translator $translator,
-        $baseTemplate
+        $baseTemplate,
+        $language
     )
     {
+        parent::__construct($viewFactory, $serviceFactory, $translator, $baseTemplate, $language);
+    }
+
+    /**
+     * Renders a template
+     *
+     * @param $template string The filename (including the path) of the template file to load
+     *
+     * @return string The rendered template
+     * @throws \PitchBlade\Mvc\View\InvalidTemplateException When the template file could not be loaded
+     */
+    public function renderMock($template)
+    {
+        return $this->render($template);
+    }
+
+    /**
+     * Renders a template
+     *
+     * @param $template string The filename (including the path) of the template file to load
+     *
+     * @return string The rendered template
+     */
+    public function renderPageMock($template)
+    {
+        return $this->renderPage($template);
     }
 }
