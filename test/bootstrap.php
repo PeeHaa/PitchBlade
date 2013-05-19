e441bd68b9dab192ff5568ec4329e6bb1ebf2fcf
PeeHaa <info@pieterhordijk.com>
2013-05-19 02:17:12 +0200

    Fixed namespacing issue

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -63,9 +63,9 @@ function getDatabaseInfo()
         'username' => 'postgres',
         'password' => '',
         'driverOptions' => [
-            PDO::ATTR_EMULATE_PREPARES   => false,
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
         ],
     ];
 
