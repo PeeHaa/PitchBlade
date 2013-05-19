3f2375cd81b929f98545e66712cc24172b55630f
PeeHaa <info@pieterhordijk.com>
2013-05-19 20:15:09 +0200

    Added mock factory

---
+++ test/Mocks/Router/RequestMatcher/Factory.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Builds the request matchers for the different types in the request
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @package    RequestMatcher
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBladeTest\Mocks\Router\RequestMatcher;
+
+use PitchBlade\Router\RequestMatcher\Buildable;
+
+/**
+ * Builds the request matchers for the different types in the request
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @package    RequestMatcher
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Factory implements Buildable
+{
+    /**
+     * Builds instances of RequestMatchers
+     *
+     * @param string $type The type of RequestMatcher to build
+     *
+     * @return \PitchBlade\Http\RequestMatcher\Matchable The instance of the matchable
+     * @throws \PitchBlade\Router\RequestMatcher\UnknownMatcherException When the matchable class doesn't exists
+     * @throws \PitchBlade\Router\RequestMatcher\InvalidMatcherException When the possible matchable class doesn't
+     *                                                                   implement the Matchable interface
+     */
+    public function build($type)
+    {
+        return new $type();
+    }
+}
