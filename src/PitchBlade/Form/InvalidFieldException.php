bffc65a1c53017688ea7a5113e1fb88a2ea3ab28
PeeHaa <info@pieterhordijk.com>
2013-05-20 23:49:57 +0200

    Added invalid field exception

---
+++ src/PitchBlade/Form/InvalidFieldException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception class for when the translation file could not be found
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    I18n
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Form;
+
+/**
+ * Exception class for when the translation file could not be found
+ *
+ * @category   PitchBlade
+ * @package    I18n
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidFieldException extends \Exception
+{
+}
