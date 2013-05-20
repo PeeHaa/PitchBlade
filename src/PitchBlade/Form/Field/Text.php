2994f29b4d0e194d8f850c636a23bcb104b6da22
PeeHaa <info@pieterhordijk.com>
2013-05-20 14:24:24 +0200

    Added text field

---
+++ src/PitchBlade/Form/Field/Text.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Text field
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Form
+ * @subpackage Field
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2012 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Form\Field;
+
+use PitchBlade\Form\Field\Generic;
+
+/**
+ * Text field
+ *
+ * @category   PitchBlade
+ * @package    Form
+ * @subpackage Field
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class Text extends Generic
+{
+    /**
+     * @var null|string The placeholder of the field
+     */
+    private $placeholder;
+
+    /**
+     * Create instance
+     *
+     * @param string $name The name of the field
+     * @param array  $data The data to construct the field
+     */
+    public function __construct($name, array $data)
+    {
+        parent::__construct($name, $data);
+
+        if (array_key_exists('placeholder', $data)) {
+            $this->placeholder = $data['placeholder'];
+        }
+
+        $this->type = 'text';
+    }
+
+    /**
+     * Get the optional placeholder
+     *
+     * @return null|string The placeholder of the field
+     */
+    public function getPlaceholder()
+    {
+        return $this->placeholder;
+    }
+}
---
+++ test/Unit/Form/Field/TextTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PitchBladeTest\Unit\Form\Field;
+
+use PitchBlade\Form\Field\Text;
+
+class TextTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers PitchBlade\Form\Field\Text::__construct
+     */
+    public function testConstructCorrectInstance()
+    {
+        $field = new Text('textField', ['placeholder' => 'placeholder']);
+
+        $this->assertInstanceOf('\\PitchBlade\\Form\\Field\\Generic', $field);
+    }
+
+    /**
+     * @covers PitchBlade\Form\Field\Text::__construct
+     * @covers PitchBlade\Form\Field\Text::getPlaceholder
+     */
+    public function testGetPlaceholder()
+    {
+        $field = new Text('textField', ['placeholder' => 'placeholderText']);
+
+        $this->assertSame('placeholderText', $field->getPlaceHolder());
+    }
+}
