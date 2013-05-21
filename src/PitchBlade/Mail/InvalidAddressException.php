4bf5dbe59e573d5b90d4983b2b55a93164519a7e
PeeHaa <info@pieterhordijk.com>
2013-05-21 20:02:16 +0200

    Added invalid address exception

---
+++ src/PitchBlade/Mail/InvalidAddressException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when trying to create an instance with an invalid e-mailaddress
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Logging
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Mail;
+
+/**
+ * Exception which gets thrown when trying to create an instance with an invalid e-mailaddress
+ *
+ * @category   PitchBlade
+ * @package    Logging
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidAddressException extends \Exception
+{
+}
