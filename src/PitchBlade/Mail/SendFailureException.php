9aedac29e107c3f129dbfaadb96bbdce23387e67
PeeHaa <info@pieterhordijk.com>
2013-05-22 23:55:24 +0200

    Added send mail failure exception

---
+++ src/PitchBlade/Mail/SendFailureException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when sending of mail failed
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Mail
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Mail;
+
+/**
+ * Exception which gets thrown when sending of mail failed
+ *
+ * @category   PitchBlade
+ * @package    Mail
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class SendFailureException extends \Exception
+{
+}
