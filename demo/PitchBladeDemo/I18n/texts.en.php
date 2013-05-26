62ed1dae671ab47553c01c00f61565a30aaa33df
PeeHaa <info@pieterhordijk.com>
2013-05-26 16:25:27 +0200

    Added footer text

--- demo/PitchBladeDemo/I18n/texts.en.php
+++ demo/PitchBladeDemo/I18n/texts.en.php
@@ -2,4 +2,5 @@
 
 $translations = [
     'site.title'        => 'PitchBlade Demo Application',
+    'footer.poweredby'  => 'Powered by <a href="https://github.com/PitchBlade" target="_blank">PitchBlade</a>',
 ];
