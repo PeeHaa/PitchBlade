70573b382974661a1b0be760da54c4a5979d968a
PeeHaa <info@pieterhordijk.com>
2013-05-26 17:34:29 +0200

    Db setup now uses the custom pdo classes for logging

--- demo/PitchBladeDemo/init.example.php
+++ demo/PitchBladeDemo/init.example.php
@@ -14,7 +14,17 @@ ini_set('date.timezone', 'Europe/Amsterdam');
 /**
  * Setup the database connection
  */
-$dbConnection = new \PDO('pgsql:dbname=pitchbladedemo;host=127.0.0.1', 'dbUsername', 'dbPassword');
-$dbConnection->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-$dbConnection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-$dbConnection->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+$dbConnection = new PDO(
+    'pgsql:dbname=pitchbladedemo;host=127.0.0.1',
+    'dbUsername',
+    'dbPassword',
+    [
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+        \PDO::ATTR_STATEMENT_CLASS    => [
+            '\\PitchBlade\\Storage\\Database\\PDOStatement',
+            [$timedLogger],
+        ],
+    ]
+);
