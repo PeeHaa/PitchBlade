54c7edc0776b82c97d53c3da33155c0ea7f72596
PeeHaa <info@pieterhordijk.com>
2013-05-27 22:05:24 +0200

    Removed demo

--- demo/PitchBladeDemo/I18n/texts.en.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$translations = [
-    'site.title'        => 'PitchBlade Demo Application',
-    'footer.poweredby'  => 'Powered by <a href="https://github.com/PitchBlade" target="_blank">PitchBlade</a>',
-];
