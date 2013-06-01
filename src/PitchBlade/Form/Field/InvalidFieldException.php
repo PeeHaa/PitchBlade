b924c35af3abd08a312ad471a2f99aaa2abf25bb
PeeHaa <info@pieterhordijk.com>
2013-06-01 20:01:56 +0200

    Added exceptions

---
+++ src/PitchBlade/Form/Field/InvalidFieldException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when the factory tries to build an invalid field
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Mvc
+ * @subpackage Model
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Form\Field;
+
+/**
+ * Exception which gets thrown when the factory tries to build an invalid field
+ *
+ * @category   PitchBlade
+ * @package    Mvc
+ * @subpackage Model
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidFieldException extends \Exception
+{
+}
---
+++ src/PitchBlade/Mvc/Model/InvalidParameterTypeException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Exception which gets thrown when the factory tries to build an invalid service
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Mvc
+ * @subpackage Model
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Mvc\Model;
+
+/**
+ * Exception which gets thrown when the factory tries to build an invalid service
+ *
+ * @category   PitchBlade
+ * @package    Mvc
+ * @subpackage Model
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidParameterTypeException extends \Exception
+{
+}
