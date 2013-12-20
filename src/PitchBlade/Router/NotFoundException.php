d80b19246865e08075f48328c1348c450f0e2087
PeeHaa <info@pieterhordijk.com>
2013-12-20 02:02:30 +0100

    Added router exceptions

---
+++ src/PitchBlade/Router/NotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when no route matches the request
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Router;
+
+/**
+ * Exception which gets thrown when no route matches the request
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class NotFoundException extends \Exception
+{
+}
---
+++ src/PitchBlade/Router/UnsupportedMethodException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when the request uses an unsupported method
+ *
+ * PHP version 5.4
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace PitchBlade\Router;
+
+/**
+ * Exception which gets thrown when the request uses an unsupported method
+ *
+ * @category   PitchBlade
+ * @package    Router
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class UnsupportedMethodException extends \Exception
+{
+}
